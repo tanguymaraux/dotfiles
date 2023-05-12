69fa1fa4b2319fe291e5a2b1fc53ea71ef00f374
Tanguy <tanguy.maraux@epita.fr>
2023-05-12 16:35:14 +0200

    add playerctl

--- install_scripts/apt_apps.sh
+++ install_scripts/apt_apps.sh
@@ -42,8 +42,10 @@ sudo make install && cd
 # Spotify
 curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
 
 # Spotify Polybar
+sudo apt install playerctl
 cd && git clone https://github.com/Jvanrhijn/polybar-spotify.git
 
 # Openvpn3
