9e08531ede09e8fe4789571405951357aeb93a1b
Tanguy <tanguy.maraux@epita.fr>
2023-11-16 10:03:07 +0100

    update powermenu script

--- scripts/rofi/powermenu.sh
+++ scripts/rofi/powermenu.sh
@@ -3,16 +3,16 @@
 
 if [ -z "$@" ]; then
     #echo -en "Suspend\0icon\x1fsystem-suspend\n"
-    #echo -en "Lock\0icon\x1fsystem-lock-screen\n"
     echo -en "Logout\0icon\x1fsystem-log-out\n"
     #echo -en "Hibernate\0icon\x1fsystem-suspend-hibernate\n"
     echo -en "Reboot\0icon\x1fsystem-reboot\n"
     echo -en "Shutdown\0icon\x1fsystem-shutdown\n"
+    echo -en "Lock\0icon\x1fsystem-lock-screen\n"
 else
     if [ "$1" = "Shutdown" ]; then
         poweroff
-    #elif [ "$1" = "Logout" ]; then
-    #    i3-msg exit
+    elif [ "$1" = "Logout" ]; then
+        i3-msg exit
     elif [ "$1" = "Reboot" ]; then
         reboot
     #elif [ "$1" = "Suspend" ]; then
@@ -24,4 +24,5 @@ else
     #    systemctl hibernate
     fi
 fi
+
 
